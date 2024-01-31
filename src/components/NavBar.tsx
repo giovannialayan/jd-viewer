6b120cb4c8789d8aa418e6e34c0d1d530fbcd6e1
giovannialayan <giovannialayan@gmail.com>
2024-01-31 16:12:56 -0500

    added github link. added some margin on stuff at the top. added songs

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,17 +7,25 @@ interface Props {
 
 function NavBar({ onNav, currentPage }: Props) {
   return (
-    <Navbar className='navbar d-flex flex-row justify-content-center'>
-      <Nav.Link className={'text-light' + (currentPage == 0 ? ' navSelected' : '')} onClick={() => onNav(0)}>
-        Song List
-      </Nav.Link>
-      <Nav.Link className={'text-light' + (currentPage == 1 ? ' navSelected' : '')} onClick={() => onNav(1)}>
-        Favorites
-      </Nav.Link>
-      <Nav.Link className={'text-light' + (currentPage == 2 ? ' navSelected' : '')} onClick={() => onNav(2)}>
-        Random
-      </Nav.Link>
-    </Navbar>
+    <div className='row navContainer'>
+      <div className='col-sm'></div>
+      <Navbar className='navbar d-flex flex-row justify-content-center col-sm'>
+        <Nav.Link className={'text-light' + (currentPage == 0 ? ' navSelected' : '')} onClick={() => onNav(0)}>
+          Song List
+        </Nav.Link>
+        <Nav.Link className={'text-light' + (currentPage == 1 ? ' navSelected' : '')} onClick={() => onNav(1)}>
+          Favorites
+        </Nav.Link>
+        <Nav.Link className={'text-light' + (currentPage == 2 ? ' navSelected' : '')} onClick={() => onNav(2)}>
+          Random
+        </Nav.Link>
+      </Navbar>
+      <div className='col-sm sideIcons d-flex flex-row justify-content-end align-content-center'>
+        <a href='https://github.com/giovannialayan/jd-viewer'>
+          <img className='sideIconImage' src='github.svg'></img>
+        </a>
+      </div>
+    </div>
   );
 }
 
